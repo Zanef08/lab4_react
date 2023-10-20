dc6448aacc8bae553a8198784a329f9b3b6f98d5
Zane <nhiphm302@gmail.com>
2023-10-20 14:05:08 +0700

    3rd

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Film } from "../shared/ListOfFilms";
 
+
 function Films() {
-  // const [film, setFilm] = useState([]);
+  const [film, setFilm] = useState([]);
   return (
     <div className="container">
       <h2>List of Films</h2> {" "}
@@ -14,11 +15,21 @@ function Films() {
           <h3 className="film-title">{film.title}</h3>
           <p className="film-year">{film.year}</p>
           <p className="film-nation">{film.nation}</p>
-          <p className="film-button">
-            {/* <a href="#popup1" id="openPopUp"><button className="btn btn-success" onClick={() => { setFilm(film); }}>Watch</button></a> */}
+          <p className="film-button" onClick={() => { setFilm(film) }}>
+            <a href='#popup1' id='openPopUp'><button className="btn btn-success">Detail</button></a>
           </p>
         </div>
       ))}
+        <div id="popup1" className="overlay">
+          <div className="popup">
+            <img className="film-image" src={film.image} alt="" />
+            <h3 className="film-title">{film.title}</h3>
+            <a className="close" href="#">X</a>
+            <div className="info">
+              {film.info}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
